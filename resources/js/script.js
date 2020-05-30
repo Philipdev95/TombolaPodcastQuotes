c3919ef22f2385488e8e46883d161b7d65022a67
Simon Egersand <s.egersand@gmail.com>
2020-05-30 15:36:39 +0200

    Add global boolean for dev mode

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,13 @@
+// Set IS_DEV if running locally
+window.IS_DEV = window.origin.includes('localhost') || window.origin.includes('127.0.0.1');
+
+if (window.IS_DEV) {
+  console.log('%cDEV MODE ENABLED!',
+      'font-weight: bold; font-size: 40px; color: red; text-shadow: 3px 3px 0 rgb(217,31,38),' +
+      ' 6px 6px 0 rgb(226,91,14), 9px 9px 0 rgb(245,221,8), 12px 12px 0 rgb(5,148,68),' +
+      ' 15px 15px 0 rgb(2,135,206), 18px 18px 0 rgb(4,77,145), 21px 21px 0 rgb(42,21,113)');
+}
+
 var allQuotes = {};
 var activeQuote = {};
 var dbRef;
