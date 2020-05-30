8fe66b9ff3c99cbbb947f942a6a172974907ee3c
Simon Egersand <s.egersand@gmail.com>
2020-05-30 18:49:09 +0200

    Break out episode list JS to separate file
    This is a big refactor.
    * Put all episode list page JS into separate file
    * Don't fetch the feed url on every load as it might never change, this
      also speeds up the load time
    * Add loading indicator
    * ES6ify the code by using promises etc.
    * Make code more readable
    
    I verified by loading the page and searching the page using the two
    different search types

---
+++ resources/js/episodeList.js
@@ -0,0 +1,196 @@
+// Set IS_DEV if running locally
+window.IS_DEV = window.origin.includes('localhost') || window.origin.includes('127.0.0.1');
+
+if (window.IS_DEV) {
+  console.log('%cYOU\'RE IN DEV MODE',
+    'font-weight: bold; font-size: 40px; color: red; text-shadow: 3px 3px 0 rgb(217,31,38),' +
+        ' 6px 6px 0 rgb(226,91,14), 9px 9px 0 rgb(245,221,8), 12px 12px 0 rgb(5,148,68),' +
+        ' 15px 15px 0 rgb(2,135,206), 18px 18px 0 rgb(4,77,145), 21px 21px 0 rgb(42,21,113)');
+}
+
+const EPISODE_SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSgkUq7i7Lz9yxAgpQQ84LdH3gB5_H23231r20_DxkeVK3jwYXN_-yGVXHwYv4ARz2qNU3Yga_T1qgv/pub?output=csv';
+const FEED_URL = 'http://tombola.libsyn.com/rss';
+
+const toTwoDigits = number => {
+  if (number < 10) {
+    return `0${number}`;
+  }
+
+  return number;
+};
+
+const getColorClass = rating => {
+  if (rating < 50) {
+    return 'bg-warning';
+  } else if (rating >= 50 && rating < 100) {
+    return 'bg-success';
+  }
+
+  // Rating === 100
+  return 'bg-info';
+};
+
+const hideLoadingSpinner = () => {
+  $('#loading-spinner').hide();
+};
+
+const xmlToJson = xml => {
+  // Taken from https://davidwalsh.name/convert-xml-json
+  let result = {};
+
+  if (xml.nodeType === 1) { // element
+    // do attributes
+    if (xml.attributes.length > 0) {
+      result['@attributes'] = {};
+      for (let j = 0; j < xml.attributes.length; j++) {
+        const attribute = xml.attributes.item(j);
+        result['@attributes'][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType === 3) { // text
+    result = xml.nodeValue;
+  }
+
+  // do children
+  // If just one text node inside
+  if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
+    result = xml.childNodes[0].nodeValue;
+  } else if (xml.hasChildNodes()) {
+    for (let i = 0; i < xml.childNodes.length; i++) {
+      const item = xml.childNodes.item(i);
+      const nodeName = item.nodeName;
+      if (typeof (result[nodeName]) === 'undefined') {
+        result[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof (result[nodeName].push) === 'undefined') {
+          const old = result[nodeName];
+          result[nodeName] = [];
+          result[nodeName].push(old);
+        }
+        result[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+
+  return result;
+};
+
+const filterSearch = () => {
+  let episodeMatch;
+  let match;
+  const filterValue = document.getElementById('searchInput').value;
+  const episodes = document.getElementById('episodeList').getElementsByTagName('li');
+  const searchFilterType = document.getElementById('search-field-select').value;
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (let i = 0; i < episodes.length; i++) {
+    if (searchFilterType === 'episode_title_numb_date') {
+      episodeMatch = episodes[i].getElementsByTagName('a')[0];
+    } else if (searchFilterType === 'episode_comments') {
+      episodeMatch = episodes[i].getElementsByClassName('show-notes')[0];
+    }
+    if (!!episodeMatch) {
+      match = episodeMatch.textContent || episodeMatch.innerText || '';
+    }
+    if (match.toUpperCase().includes(filterValue.toUpperCase())) {
+      episodes[i].classList.add('d-block');
+      episodes[i].style.display = '';
+    } else {
+      episodes[i].classList.remove('d-block');
+      episodes[i].style.display = 'none';
+    }
+  }
+};
+
+const fetchEpisodeMetadata = () => {
+  const url = window.IS_DEV ? `${window.origin}/resources/mock/episodes-response.txt` : EPISODE_SPREADSHEET_URL;
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url,
+      error: error => {
+        console.error('Error: There was a problem processing your request, please refresh the browser and try again', error);
+        // Display error message
+        $('.status-message').html('Något gick fel. Prova att ladda om sidan. <button class="btn btn-sm btn-info" onClick="history.go(0);">Ladda om</button>');
+        reject(error);
+      },
+      success: data => resolve(data.split('\n')),
+    });
+  });
+};
+
+const fetchEpisodesFromFeed = episodesMetadata => {
+  return new Promise((resolve, reject) =>
+    $.ajax({
+      type: 'GET',
+      url: FEED_URL,
+      dataType: 'xml',
+      error: error => {
+        console.error('Error: There was a problem processing your request, please refresh the browser and try again', error);
+        reject(error);
+      },
+      success: data => resolve({episodes: data, metadata: episodesMetadata}),
+    }),
+  );
+};
+
+const displayEpisodes = (episodes, metadata) => {
+  const jsonResponse = xmlToJson(episodes);
+  const titles = jsonResponse.rss.channel.item.reverse();
+
+  $.each(titles, (index, title) => {
+    // Skip first element
+    if (index === 0) {
+      return;
+    }
+
+    const episode = metadata[index].split(',');
+    let showNotes = '';
+    let skojPoints = '';
+    let carousel = '';
+    const episodeNotes = episode[4];
+    const episodeRating = episode[3];
+    const epiShowQuotes = episode[5];
+    if (!!episodeNotes) {
+      showNotes = `<hr class="m-1"><i class="show-notes text-muted">${episodeNotes}</i>`;
+    }
+    if (!!episodeRating) {
+      const colorClass = getColorClass(episodeRating);
+      skojPoints = `<hr class="m-1"><div class="progress"><div class="progress-bar ${colorClass}" role="progressbar" style="width: ${episodeRating}%;" aria-valuenow="${episodeRating}" aria-valuemin="0" aria-valuemax="100">Skojfaktor: ${episodeRating}</div></div>`;
+    }
+    if (!!epiShowQuotes) {
+      const quotes = epiShowQuotes.split(';');
+      let prev = '';
+      let next = '';
+      let item;
+      let allQuotes = '';
+      $.each(quotes, (i, quote) => {
+        if (i === 0) {
+          item = `<div class="carousel-item active" data-interval="4000">${quote}</div>`;
+        } else {
+          item = `<div class="carousel-item" data-interval="4000">${quote}</div>`;
+          prev = `<a class="carousel-control-prev" href="#quoteCarousel${index}" role="button" data-slide="prev"><i class="text-dark far fa-chevron-left"></i><span class="sr-only">Previous</span></a>`;
+          next = `<a class="carousel-control-next" href="#quoteCarousel${index}" role="button" data-slide="next"><i class="text-dark far fa-chevron-right"></i><span class="sr-only">Next</span></a>`;
+        }
+        allQuotes = allQuotes + item;
+      });
+      const inner = `<div class="carousel-inner">${allQuotes}</div>`;
+      carousel = `<hr class="m-1"><div id="quoteCarousel${index}" class="carousel slide" data-ride="carousel">${inner}${prev}${next}</div>`;
+    }
+    const pubDate = new Date(title.pubDate);
+    const date = `${pubDate.getFullYear()}.${toTwoDigits(pubDate.getMonth() + 1)}.${toTwoDigits(pubDate.getDate())}`;
+    $('#episodeList').prepend(
+      `<li href="#" data-index="${index}" class="list-group-item list-group-item-action d-block">
+                <a class="d-flex justify-content-between">
+                    <span>${title.title}</span><span>${date}</span>
+                </a>${showNotes}${skojPoints}${carousel}
+             </li>`,
+    );
+  });
+};
+
+fetchEpisodeMetadata()
+  .then(fetchEpisodesFromFeed)
+  .then(({episodes, metadata}) => {
+    hideLoadingSpinner();
+    displayEpisodes(episodes, metadata);
+  });
--- resources/js/script.js
+++ resources/js/script.js
@@ -14,7 +14,6 @@ let dbRef;
 let highscore = [];
 let isFirstGame = true;
 
-/*-----------------*/
 const sortHighScore = newScore => {
   highscore.push(newScore);
 
@@ -46,19 +45,9 @@ const getAllQuotes = () => {
   });
 };
 
-const getTwoDigits = number => {
-  if (number < 10) {
-    return `0${number}`;
-  }
-
-  return number;
-};
-
-/*------*/
 $('#allQuotes').click(() => {
   getAllQuotes();
 });
-/*-----------------*/
 const getQuote = name => {
   const quotes = allQuotes[name].quotes;
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
@@ -69,13 +58,11 @@ const getQuote = name => {
   };
 };
 
-/*-----------------*/
 const printPoints = () => {
   // Print stored points
   $('#display-points').html(localStorage.getItem('storedPoints'));
 };
 
-/*-----------------*/
 const addPoints = () => {
   // checking if there are saved points in storage
   let storedPoints = parseInt(localStorage.getItem('storedPoints'), 10);
@@ -87,7 +74,6 @@ const addPoints = () => {
   }
 };
 
-/*-----------------*/
 const getRandomPerson = () => {
   // choosing from who you get the quote
   // if 1 choose carl
@@ -97,12 +83,10 @@ const getRandomPerson = () => {
   return 'marcus';
 };
 
-/*-----------------*/
 const printQuote = () => {
   $('#quotes .quote-text').text(activeQuote.quote);
 };
 
-/*-----------------*/
 const presentAnswer = guess => {
   // remove question and show result(right vs wrong)
   $('#question').addClass('hidden');
@@ -118,7 +102,6 @@ const presentAnswer = guess => {
   }
 };
 
-/*-----------------*/
 const newQuote = () => {
   // reset the visibility
   $('#question').removeClass('hidden');
@@ -133,7 +116,6 @@ const newQuote = () => {
   printQuote();
 };
 
-/*-----------------*/
 const startGame = () => {
   localStorage.setItem('storedPoints', 0);
   $('#end').addClass('hidden');
@@ -157,7 +139,6 @@ const startGame = () => {
   }
 };
 
-/*----------------*/
 const endGame = () => {
   $('#question').addClass('hidden');
   $('#answer').addClass('hidden');
@@ -171,7 +152,6 @@ const endGame = () => {
   }
 };
 
-/*-----------------*/
 const saveHighscore = () => {
   $('#add-form').addClass('hidden');
 
@@ -183,7 +163,6 @@ const saveHighscore = () => {
   }
 };
 
-/*-----------------*/
 const printHighscore = () => {
   highscore.forEach(({name, points}, i) => {
     $(`#toplist-${i} .name`).text(name);
@@ -191,7 +170,6 @@ const printHighscore = () => {
   });
 };
 
-/*-----------------*/
 $(document).ready(() => {
   // get quotes
   $.getJSON('../resources/json/quotes.json', quotes => {
@@ -229,158 +207,3 @@ $(document).ready(() => {
     startGame();
   });
 });
-
-const xmlToJson = xml => {
-  // Create the return object
-  let obj = {};
-
-  if (xml.nodeType === 1) { // element
-    // do attributes
-    if (xml.attributes.length > 0) {
-      obj['@attributes'] = {};
-      for (let j = 0; j < xml.attributes.length; j++) {
-        const attribute = xml.attributes.item(j);
-        obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
-      }
-    }
-  } else if (xml.nodeType === 3) { // text
-    obj = xml.nodeValue;
-  }
-
-  // do children
-  // If just one text node inside
-  if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
-    obj = xml.childNodes[0].nodeValue;
-  } else if (xml.hasChildNodes()) {
-    for (let i = 0; i < xml.childNodes.length; i++) {
-      const item = xml.childNodes.item(i);
-      const nodeName = item.nodeName;
-      if (typeof(obj[nodeName]) === 'undefined') {
-        obj[nodeName] = xmlToJson(item);
-      } else {
-        if (typeof(obj[nodeName].push) === 'undefined') {
-          const old = obj[nodeName];
-          obj[nodeName] = [];
-          obj[nodeName].push(old);
-        }
-        obj[nodeName].push(xmlToJson(item));
-      }
-    }
-  }
-
-  return obj;
-};
-
-const getXML = feedUrl => {
-  let ssArr; let url;
-  if (window.IS_DEV) {
-    url = `${window.origin}/resources/mock/episodes-response.txt`;
-  } else {
-    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSgkUq7i7Lz9yxAgpQQ84LdH3gB5_H23231r20_DxkeVK3jwYXN_-yGVXHwYv4ARz2qNU3Yga_T1qgv/pub?output=csv';
-  }
-  $.ajax({
-    url,
-    success: data => {
-      ssArr = data.split('\n');
-    },
-  });
-  $.ajax({
-    type: 'GET',
-    url: feedUrl,
-    dataType: 'xml',
-    error: _response => {
-      console.error('Error: There was a problem processing your request, please refresh the browser and try again');
-    },
-    success: response => {
-      const XMLisJSON = xmlToJson(response);
-      const titles = XMLisJSON.rss.channel.item.reverse();
-      $.each(titles, (index, title) => {
-        const statusMsg = $('.status-message');
-
-        if (ssArr === undefined) {
-          statusMsg.html('Något gick fel. Pröva att ladda om sidan. <button class="btn btn-sm" onClick="history.go(0);">Refresh Page</button>');
-          return false;
-        }
-        statusMsg.html('');
-
-        const item = ssArr[index].split(',');
-        let showNotes = '';
-        let skojPoints = '';
-        let carousel = '';
-        const gs_showRated = item[3];
-        const gs_showNotes = item[4];
-        const gs_showQuotes = item[5];
-        if (index !== 0) {
-          if (item[4] !== '') {
-            showNotes = `<hr class="m-1"><i class="show-notes text-muted">${gs_showNotes}</i>`;
-          }
-          if (gs_showRated !== '') {
-            let colorClass;
-            if (gs_showRated < 50) {colorClass = 'bg-warning';} else if (gs_showRated >= 50 && gs_showRated < 100) {colorClass = 'bg-success';} else if (gs_showRated === 100) {colorClass = 'bg-info';}
-            skojPoints = `<hr class="m-1"><div class="progress"><div class="progress-bar ${colorClass}" role="progressbar" style="width: ${gs_showRated}%;" aria-valuenow="${gs_showRated}" aria-valuemin="0" aria-valuemax="100">Skojfaktor: ${gs_showRated}</div></div>`;
-          }
-          if (gs_showQuotes !== '') {
-            const quotes = gs_showQuotes.split(';');
-            let prev = '';
-            let next = '';
-            let item;
-            let allQuotes = '';
-            $.each(quotes, (i, quote) => {
-              if (i === 0) {
-                item = `<div class="carousel-item active" data-interval="4000">${quote}</div>`;
-              } else {
-                item = `<div class="carousel-item" data-interval="4000">${quote}</div>`;
-                prev = `<a class="carousel-control-prev" href="#quoteCarousel${index}" role="button" data-slide="prev"><i class="text-dark far fa-chevron-left"></i><span class="sr-only">Previous</span></a>`;
-                next = `<a class="carousel-control-next" href="#quoteCarousel${index}" role="button" data-slide="next"><i class="text-dark far fa-chevron-right"></i><span class="sr-only">Next</span></a>`;
-              }
-              allQuotes = allQuotes + item;
-            });
-            const inner = `<div class="carousel-inner">${allQuotes}</div>`;
-            carousel = `<hr class="m-1"><div id="quoteCarousel${index}" class="carousel slide" data-ride="carousel">${inner}${prev}${next}</div>`;
-          }
-          const date = `${new Date(title.pubDate).getFullYear()}.${getTwoDigits(new Date(title.pubDate).getMonth() + 1)}.${getTwoDigits(new Date(title.pubDate).getDate())}`;
-          $('#episodeList').prepend(`<li href="#" data-index="${index}" class="list-group-item list-group-item-action d-block"><a class="d-flex justify-content-between"><span>${title.title}</span><span>${date}</span></a>${showNotes}${skojPoints}${carousel}</li>`);
-        }
-      });
-    },
-  });
-};
-
-$.ajax({
-  type: 'post',
-  url: 'https://itunes.apple.com/lookup?id=1095020110&entity=podcast',
-  cache: false,
-  success: results => {
-    const data = JSON.parse(results);
-    getXML(data.results[0].feedUrl);
-  },
-  error: ({responseText}) => {
-    console.error(JSON.parse(responseText));
-  },
-});
-
-const filterSearch = () => {
-  let episodeMatch; let match;
-  const filterValue = document.getElementById('searchInput').value;
-  const episodes = document.getElementById('episodeList').getElementsByTagName('li');
-  const searchFilterType = document.getElementById('search-field-select').value;
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (let i = 0; i < episodes.length; i++) {
-    if (searchFilterType === 'episode_title_numb_date') {
-      episodeMatch = episodes[i].getElementsByTagName('a')[0];
-    } else if (searchFilterType === 'episode_comments') {
-      episodeMatch = episodes[i].getElementsByClassName('show-notes')[0];
-    }
-    if (!!episodeMatch) {
-      match = episodeMatch.textContent || episodeMatch.innerText || '';
-    }
-    if (match.toUpperCase().includes(filterValue.toUpperCase())) {
-      episodes[i].classList.add('d-block');
-      episodes[i].style.display = '';
-    } else {
-      episodes[i].classList.remove('d-block');
-      episodes[i].style.display = 'none';
-    }
-  }
-};
